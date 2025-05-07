d8530c2a8501ae15045bc635073ef48d5e0f9067
Suyash Kumar <152602944+sparrowjack234@users.noreply.github.com>
2025-05-07 11:29:56 +0530

    Update training.py

--- training.py
+++ training.py
@@ -1,234 +1,166 @@
 """
-Training the model
-Extended from original implementation of PANet by Wang et al.
+Experiment configuration file
+Extended from config file from original PANet Repository
 """
 import os
-import shutil
-import torch
-import torch.nn as nn
-import torch.optim
-from torch.utils.data import DataLoader
-from torch.optim.lr_scheduler import MultiStepLR
-import torch.backends.cudnn as cudnn
-import numpy as np
+import re
+import glob
+import itertools
 
-from models.grid_proto_fewshot import FewShotSeg
-from dataloaders.dev_customized_medv1 import med_fewshot
-from dataloaders.GenericSuperDatasetv1 import SuperpixelDataset
-from dataloaders.dataset_utils import DATASET_INFO
-import dataloaders.augutils as myaug
+import sacred
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
+from sacred.utils import apply_backspaces_and_linefeeds
 
-from util.utils import set_seed, t2n, to01, compose_wt_simple, get_tversky_loss
-from util.metric import Metric
+from platform import node
+from datetime import datetime
 
-from config_ssl_upload import ex
-import tqdm
-import time
+sacred.SETTINGS['CONFIG']['READ_ONLY_CONFIG'] = False
+sacred.SETTINGS.CAPTURE_MODE = 'no'
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+ex = Experiment('mySSL')
+ex.captured_out_filter = apply_backspaces_and_linefeeds
 
-# config pre-trained model caching path
-os.environ['TORCH_HOME'] = "./pretrained_model"
+source_folders = ['.', './dataloaders', './models', './util']
+sources_to_save = list(itertools.chain.from_iterable(
+    [glob.glob(f'{folder}/*.py') for folder in source_folders]))
+for source_file in sources_to_save:
+    ex.add_source_file(source_file)
 
-@ex.automain
-def main(_run, _config, _log):
-    if _run.observers:
-        os.makedirs(f'{_run.observers[0].dir}/snapshots', exist_ok=True)
-        os.makedirs(f'{_run.observers[0].dir}/trainsnaps', exist_ok=True)
-        for source_file, _ in _run.experiment_info['sources']:
-            os.makedirs(os.path.dirname(f'{_run.observers[0].dir}/source/{source_file}'),
-                        exist_ok=True)
-            _run.observers[0].save_file(source_file, f'source/{source_file}')
-        shutil.rmtree(f'{_run.observers[0].basedir}/_sources')
+@ex.config
+def cfg():
+    """Default configurations"""
+    seed = 1234
+    gpu_id = 0
+    mode = 'train' # for now only allows 'train' 
+    num_workers = 4 # 0 for debugging. 
 
-    set_seed(_config['seed'])
-    cudnn.enabled = True
-    cudnn.benchmark = True
-    torch.cuda.set_device(device=_config['gpu_id'])
-    torch.set_num_threads(1)
+    dataset = 'SABS_Superpix' # i.e. abdominal MRI
+    use_coco_init = True # initialize backbone with MS_COCO initialization. Anyway coco does not contain medical images
 
-    _log.info('###### Create model ######')
+    ### Training
+    n_steps = 100100
+    batch_size = 1
+    lr_milestones = [ (ii + 1) * 1000 for ii in range(n_steps // 1000 - 1)]
+    lr_step_gamma = 0.95
+    ignore_label = 255
+    print_interval = 500
+    save_snapshot_every = 25000
+    max_iters_per_load = 1000 # epoch size, interval for reloading the dataset
+    scan_per_load = -1 # numbers of 3d scans per load for saving memory. If -1, load the entire dataset to the memory
+    which_aug = 'sabs_aug' # standard data augmentation with intensity and geometric transforms
+    input_size = (256, 256)
+    min_fg_data='100' # when training with manual annotations, indicating number of foreground pixels in a single class single slice. This empirically stablizes the training process
+    label_sets = 0 # which group of labels taking as training (the rest are for testing)
+    exclude_cls_list = [2, 3] # testing classes to be excluded in training. Set to [] if testing under setting 1
+    usealign = True # see vanilla PANet
+    use_wce = True
+    viz = 1
 
-    model = FewShotSeg(pretrained_path=None, cfg=_config['model'])
+    ### Validation
+    z_margin = 0 
+    eval_fold = 0 # which fold for 5 fold cross validation
+    support_idx=[4] # indicating which scan is used as support in testing. 
+    val_wsize=2 # L_H, L_W in testing
+    n_sup_part = 3 # number of chuncks in testing
+    # Add these to your config function in config_ssl_upload.py
+    run_param_tuning = True  # Whether to run parameter tuning experiments
+    use_best_params = True  # Whether to use the best parameters from tuning
 
-    model = model.cuda()
-    model.train()
+    # Network
+    modelname = 'dlfcn_res101' # resnet 101 backbone from torchvision fcn-deeplab
+    clsname = 'grid_proto' # 
+    resume = False
+    reload_model_path = './runs/mySSL__CHAOST2_Superpix_sets_0_1shot/48/snapshots/100000.pth' # path for reloading a trained model (overrides ms-coco initialization)
+    proto_grid_size = 8 # L_H, L_W = (32, 32) / 8 = (4, 4)  in training
+    feature_hw = [32, 32] # feature map size, should couple this with backbone in future
 
-    _log.info('###### Load data ######')
-    ### Training set
-    data_name = _config['dataset']
-    if data_name == 'SABS_Superpix':
-        baseset_name = 'SABS'
-    elif data_name == 'C0_Superpix':
-        raise NotImplementedError
-        baseset_name = 'C0'
-    elif data_name == 'CHAOST2_Superpix':
-        baseset_name = 'CHAOST2'
-    else:
-        raise ValueError(f'Dataset: {data_name} not found')
+    # SSL
+    superpix_scale = 'MIDDLE' #MIDDLE/ LARGE
 
-    ###================== Transforms for data augmentation =============================== ###
-    tr_transforms = myaug.transform_with_label({'aug': myaug.augs[_config['which_aug']]})
-    ### ================================================================================== ###
-    assert _config['scan_per_load'] < 0 # by default we load the entire dataset directly
+    tversky_params = {'tversky_alpha' : 0.3,
+                    'tversky_beta' : 0.7,
+                    'tversky_gamma' : 1.0}
 
-    test_labels = DATASET_INFO[baseset_name]['LABEL_GROUP']['pa_all'] - DATASET_INFO[baseset_name]['LABEL_GROUP'][_config["label_sets"]]
-    _log.info(f'###### Labels excluded in training : {[lb for lb in _config["exclude_cls_list"]]} ######')
-    _log.info(f'###### Unseen labels evaluated in testing: {[lb for lb in test_labels]} ######')
+    lambda_loss = {'loss1':0.0, 'loss2':1.0, 'loss3':0.0, 'loss4':0.0, 'loss5': 0.0}
 
-    tr_parent = SuperpixelDataset( # base dataset
-        which_dataset = baseset_name,
-        base_dir=_config['path'][data_name]['data_dir'],
-        idx_split = _config['eval_fold'],
-        mode='train',
-        min_fg=str(_config["min_fg_data"]), # dummy entry for superpixel dataset
-        transforms =tr_transforms, #
-        transform_param_limits = myaug.augs[_config['which_aug']],#tr_transforms, #
-        nsup = _config['task']['n_shots'],
-        scan_per_load = _config['scan_per_load'],
-        exclude_list = _config["exclude_cls_list"],
-        superpix_scale = _config["superpix_scale"],
-        fix_length = _config["max_iters_per_load"] if (data_name == 'C0_Superpix') or (data_name == 'CHAOST2_Superpix') else None,
-        dataset_config = _config['DATASET_CONFIG']
-    )
+    accum_iter = 1
 
-    ### dataloaders
-    trainloader = DataLoader(
-        tr_parent,
-        batch_size=_config['batch_size'],
-        shuffle=True,
-        num_workers=_config['num_workers'],
-        pin_memory=True,
-        drop_last=True
-    )
 
-    _log.info('###### Set optimizer ######')
-    if _config['optim_type'] == 'sgd':
-        print("Using SGD optimizer")
-        optimizer = torch.optim.SGD(model.parameters(), **_config['optim'])
-    elif _config['optim_type'] == 'adam':
-        print("Using ADAM optimizer")
-        optimizer = torch.optim.Adam(model.parameters(), **_config['optim'])
-    else:
-        raise NotImplementedError
+    model = {
+        'align': usealign,
+        'use_coco_init': use_coco_init,
+        'which_model': modelname,
+        'cls_name': clsname,
+        'proto_grid_size' : proto_grid_size,
+        'feature_hw': feature_hw,
+        'reload_model_path': reload_model_path,
+        'use_refinement': True,
+        'refinement_iterations': 3,
+        'use_feedback': True,
+        'small_object_threshold': 0.01, 
+        'run_param_tuning' : True,
+        'use_best_params' : True  
+    }
 
-    # optimizer.load_state_dict(torch.load(_config['reload_model_path'])['opt'])
+    task = {
+        'n_ways': 1,
+        'n_shots': 1,
+        'n_queries': 1,
+        'npart': n_sup_part 
+    }
 
-    scheduler = MultiStepLR(optimizer, milestones=_config['lr_milestones'],  gamma = _config['lr_step_gamma'])
-    # scheduler.load_state_dict(torch.load(_config['reload_model_path'])['sch'])
+    optim_type = 'sgd'
+    optim = {
+        'lr': 1e-3, 
+        'momentum': 0.9,
+        'weight_decay': 0.0005,
+    }
 
-    my_weight = compose_wt_simple(_config["use_wce"], data_name)
-    criterion = nn.CrossEntropyLoss(ignore_index=_config['ignore_label'], weight = my_weight)
+    exp_prefix = ''
 
-    i_iter = 0 # total number of iteration
-    n_sub_epoches = _config['n_steps'] // _config['max_iters_per_load'] # number of times for reloading
+    exp_str = '_'.join(
+        [exp_prefix]
+        + [dataset,]
+        + [f'sets_{label_sets}_{task["n_shots"]}shot'])
 
-    log_loss = {'loss': 0, 'align_loss': 0}
+    path = {
+        'log_dir': './runs',
+        'SABS':{'data_dir': "E:\Suyash\cowpro\data\SABS\sabs_CT_normalized"
+            },
+        'C0':{'data_dir': "E:\Suyash\cowpro\data"
+            },
+        'CHAOST2':{'data_dir': "E:\Suyash\cowpro\data\CHAOST2\chaos_MR_T2_normalized"
+            },
+        'FLARE22Train':{'data_dir':"E:\Suyash\cowpro\data\FLARE22Train\flare_CT_normalized"
+            },
+        'SABS_Superpix':{'data_dir': "E:\Suyash\cowpro\data\SABS\sabs_CT_normalized"},
+        'C0_Superpix':{'data_dir': "E:\Suyash\cowpro\data"},
+        'CHAOST2_Superpix':{'data_dir': "E:\Suyash\cowpro\data\CHAOST2\chaos_MR_T2_normalized"},
+        'FLARE22Train_Superpix':{'data_dir':"E:\Suyash\cowpro\data\FLARE22Train\flare_CT_normalized"}
+        }
 
-    _log.info('###### Training ######')
-    stime = time.time()
-    for sub_epoch in range(n_sub_epoches):
-        _log.info(f'###### This is epoch {sub_epoch} of {n_sub_epoches} epoches ######')
-        for _, sample_batched in enumerate(trainloader):
-            # Prepare input
-            i_iter += 1
-            # add writers
-            support_images = [[shot.float().cuda() for shot in way]
-                              for way in sample_batched['support_images']]
-            support_fg_mask = [[shot[f'fg_mask'].float().cuda() for shot in way]
-                               for way in sample_batched['support_mask']]
-            support_bg_mask = [[shot[f'bg_mask'].float().cuda() for shot in way]
-                               for way in sample_batched['support_mask']]
+    DATASET_CONFIG = {'SABS':{'img_bname': f'E:\Suyash\cowpro\data\SABS\sabs_CT_normalized/image_*.nii.gz',
+                        'out_dir': 'E:\Suyash\cowpro\data\SABS\sabs_CT_normalized',
+                        'fg_thresh': 1e-4,
+                        },
+                      'CHAOST2':{
+                       'img_bname': f'E:\Suyash\cowpro\data\CHAOST2\chaos_MR_T2_normalized/image_*.nii.gz',
+                          'out_dir': 'E:\Suyash\cowpro\data\CHAOST2\chaos_MR_T2_normalized',
+                          'fg_thresh': 1e-4 + 50,
+                        },
+                        'FLARE22Train':{
+                         'img_bname': f'E:\Suyash\cowpro\data\FLARE22Train\flare_CT_normalized/image_*.nii.gz',
+                          'out_dir': 'E:\Suyash\cowpro\data\FLARE22Train\flare_CT_normalized',
+                          'fg_thresh': 1e-4                     
+                        },
+                     }
 
-            query_images = [query_image.float().cuda()
-                            for query_image in sample_batched['query_images']]
-            query_labels = torch.cat(
-                [query_label.long().cuda() for query_label in sample_batched['query_labels']], dim=0)
 
-            optimizer.zero_grad()
-            # FIXME: in the model definition, filter out the failure case where 
-            # pseudolabel falls outside of image or too small to calculate a prototype
-            # try:
-            mean = [m for m in sample_batched["mean"]]
-            std = [s for s in sample_batched["std"]]
-            ########################################################################
-            query_pred, align_loss, debug_vis, assign_mats = model(support_images,
-                                                                    support_fg_mask, 
-                                                                    support_bg_mask, 
-                                                                    query_images, 
-                                                                    isval = False, val_wsize = None)
-            ########################################################################
-            # except:
-                # print('Faulty batch detected, skip')
-                # continue
-
-            query_loss = criterion(query_pred, query_labels) + get_tversky_loss(query_pred.argmax(dim = 1, keepdim = True), query_labels[None, ...], 0.3, 0.7 ,1.0)
-            loss = query_loss + align_loss
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-
-            # Log loss
-            query_loss = query_loss.detach().data.cpu().numpy()
-            align_loss = align_loss.detach().data.cpu().numpy() if align_loss != 0 else 0
-
-            _run.log_scalar('loss', query_loss)
-            _run.log_scalar('align_loss', align_loss)
-            log_loss['loss'] += query_loss
-            log_loss['align_loss'] += align_loss
-
-            # print loss and take snapshots
-            if (i_iter + 1) % _config['print_interval'] == 0:
-
-                nt = time.time()
-
-                loss = log_loss['loss'] / _config['print_interval']
-                align_loss = log_loss['align_loss'] / _config['print_interval']
-
-                log_loss['loss'] = 0
-                log_loss['align_loss'] = 0
-
-                fig, ax = plt.subplots(1,2)
-                # print(mean.shape, std.shape, mean, std)            
-                # print(support_images[0][0][0].min(), support_images[0][0][0].max())
-                si = (support_images[0][0][0].cpu()*std[0]+mean[0]).numpy().transpose((1,2,0))
-                si = (si - si.min())/(si.max() - si.min() + 1e-6)
-                # print(si.shape)
-                ax[0].imshow(si)
-                # print(support_fg_mask[0][0][0].shape)
-                sm = support_fg_mask[0][0][0].cpu().numpy()
-                # print(sm.shape)
-                ax[0].imshow(np.stack([np.zeros(sm.shape),sm,np.zeros(sm.shape)], axis = 2), alpha = 0.3)
-
-                # print(query_pred[0].shape)
-                qi = (query_images[0][0].cpu()*std[0]+mean[0]).numpy().transpose((1,2,0))
-                qi = (qi - qi.min())/(qi.max() - qi.min() + 1e-6)
-                ax[1].imshow(qi)
-                qm = query_labels[0].cpu().numpy()
-                # print(sm.shape, qm.shape)
-                ax[1].imshow(np.stack([np.zeros(qm.shape),qm,np.zeros(qm.shape)], axis = 2), alpha = 0.3)
-                qp = query_pred.argmax(dim = 1).float().cpu().numpy()[0] #(query_pred > 0.5)[0][0].cpu().numpy() #.transpose((1,2,0))
-                # print(qp.shape)
-                ax[1].imshow(np.stack([qp,np.zeros(qp.shape),np.zeros(qp.shape)], axis = 2), alpha = 0.2)
-
-                plt.savefig(os.path.join(f'{_run.observers[0].dir}/trainsnaps', f'{i_iter + 1}.png'), bbox_inches='tight')
-                plt.close(fig)
-
-                print(f'step {i_iter+1}: loss: {loss}, align_loss: {align_loss}, time: {(nt-stime)/60} mins')
-
-            if (i_iter + 1) % _config['save_snapshot_every'] == 0:
-                _log.info('###### Taking snapshot ######')
-                torch.save({'model':model.state_dict(),'opt':optimizer.state_dict(),'sch':scheduler.state_dict()},
-                           os.path.join(f'{_run.observers[0].dir}/snapshots', f'{i_iter + 1}.pth'))
-
-            if data_name == 'C0_Superpix' or data_name == 'CHAOST2_Superpix':
-                if (i_iter + 1) % _config['max_iters_per_load'] == 0:
-                    _log.info('###### Reloading dataset ######')
-                    trainloader.dataset.reload_buffer()
-                    print(f'###### New dataset with {len(trainloader.dataset)} slices has been loaded ######')
-
-            if (i_iter - 2) > _config['n_steps']:
-                return 1 # finish up
-
+@ex.config_hook
+def add_observer(config, command_name, logger):
+    """A hook fucntion to add observer"""
+    exp_name = f'{ex.path}_{config["exp_str"]}'
+    observer = FileStorageObserver.create(os.path.join(config['path']['log_dir'], exp_name))
+    ex.observers.append(observer)
+    return config
